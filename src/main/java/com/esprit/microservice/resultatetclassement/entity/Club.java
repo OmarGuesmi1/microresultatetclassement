c6326bddd502f6c496ffcb682452427b431a8482
yosrmrabet <yosr.mrabet@esprit.tn>
2025-10-21 16:16:20 +0100

    use Lombok @Getter and @Setter annotations instead of manual methods in Club entity
    Co-Authored-By: omarguesmi <omar.guesmi@esprit.tn>

--- src/main/java/com/esprit/microservice/resultatetclassement/entity/Club.java
+++ src/main/java/com/esprit/microservice/resultatetclassement/entity/Club.java
@@ -1,9 +1,14 @@
 package com.esprit.microservice.resultatetclassement.entity;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "clubs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Club {
 
     @Id
@@ -13,66 +18,4 @@ public class Club {
     private String pays;
     private String stade;
     private String urlLogo;
-
-    // 🧱 Constructeurs
-    public Club() {
-    }
-
-    public Club(String id, String nom, String ville, String pays, String stade, String urlLogo) {
-        this.id = id;
-        this.nom = nom;
-        this.ville = ville;
-        this.pays = pays;
-        this.stade = stade;
-        this.urlLogo = urlLogo;
-    }
-
-    // 🧩 Getters et Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getVille() {
-        return ville;
-    }
-
-    public void setVille(String ville) {
-        this.ville = ville;
-    }
-
-    public String getPays() {
-        return pays;
-    }
-
-    public void setPays(String pays) {
-        this.pays = pays;
-    }
-
-    public String getStade() {
-        return stade;
-    }
-
-    public void setStade(String stade) {
-        this.stade = stade;
-    }
-
-    public String getUrlLogo() {
-        return urlLogo;
-    }
-
-    public void setUrlLogo(String urlLogo) {
-        this.urlLogo = urlLogo;
-    }
 }
