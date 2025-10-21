081781357bef305511db7b8a3de04d113f870a96
yosrmrabet <yosr.mrabet@esprit.tn>
2025-10-21 16:31:44 +0100

    replace manual getters/setters with Lombok and store enums as strings in Match entity
    Refactored the Match entity by removing manually implemented getters, setters,
    and constructors. Added Lombok annotations (@Getter, @Setter, @NoArgsConstructor,
    @AllArgsConstructor) to simplify the class.
    
    Also annotated enum fields (StatutMatch, VainqueurMatch, TourMatch) with
    @Field(targetType = FieldType.STRING) to ensure they are stored as plain strings
    in MongoDB instead of complex objects.
    
    Co-Authored-By: omarguesmi <omar.guesmi@esprit.tn>

--- src/main/java/com/esprit/microservice/resultatetclassement/entity/Match.java
+++ src/main/java/com/esprit/microservice/resultatetclassement/entity/Match.java
@@ -1,10 +1,18 @@
 package com.esprit.microservice.resultatetclassement.entity;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
 import java.time.LocalDate;
 
 @Document(collection = "matches")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Match {
 
     @Id
@@ -18,132 +26,15 @@ public class Match {
     private int butsExterieur;
     private LocalDate dateMatch;
 
-    private StatutMatch statut; // PROGRAMMÉ, TERMINÉ, ANNULÉ
-    private VainqueurMatch vainqueur; // DOMICILE, EXTERIEUR, ÉGALITÉ
+    @Field(targetType = FieldType.STRING) 
+    private StatutMatch statut;
+
+    @Field(targetType = FieldType.STRING)
+    private VainqueurMatch vainqueur;
 
     private String stade;
     private boolean domicile;
 
-    // ❌ Ce champ sera utilisé uniquement pour certaines compétitions
+    @Field(targetType = FieldType.STRING)
     private TourMatch tour;
-
-    // 🧱 Constructeurs
-    public Match() {}
-
-    public Match(String id, String idCompetition, String idClubDomicile, String idClubExterieur,
-                 int butsDomicile, int butsExterieur, LocalDate dateMatch,
-                 StatutMatch statut, VainqueurMatch vainqueur,
-                 String stade, boolean domicile, TourMatch tour) {
-        this.id = id;
-        this.idCompetition = idCompetition;
-        this.idClubDomicile = idClubDomicile;
-        this.idClubExterieur = idClubExterieur;
-        this.butsDomicile = butsDomicile;
-        this.butsExterieur = butsExterieur;
-        this.dateMatch = dateMatch;
-        this.statut = statut;
-        this.vainqueur = vainqueur;
-        this.stade = stade;
-        this.domicile = domicile;
-
-        // ⚡ On ne remplit tour que si c’est une compétition type Ligue des Champions
-        this.tour = tour;
-    }
-
-    // 🧩 Getters & Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getIdCompetition() {
-        return idCompetition;
-    }
-
-    public void setIdCompetition(String idCompetition) {
-        this.idCompetition = idCompetition;
-    }
-
-    public String getIdClubDomicile() {
-        return idClubDomicile;
-    }
-
-    public void setIdClubDomicile(String idClubDomicile) {
-        this.idClubDomicile = idClubDomicile;
-    }
-
-    public String getIdClubExterieur() {
-        return idClubExterieur;
-    }
-
-    public void setIdClubExterieur(String idClubExterieur) {
-        this.idClubExterieur = idClubExterieur;
-    }
-
-    public int getButsDomicile() {
-        return butsDomicile;
-    }
-
-    public void setButsDomicile(int butsDomicile) {
-        this.butsDomicile = butsDomicile;
-    }
-
-    public int getButsExterieur() {
-        return butsExterieur;
-    }
-
-    public void setButsExterieur(int butsExterieur) {
-        this.butsExterieur = butsExterieur;
-    }
-
-    public LocalDate getDateMatch() {
-        return dateMatch;
-    }
-
-    public void setDateMatch(LocalDate dateMatch) {
-        this.dateMatch = dateMatch;
-    }
-
-    public StatutMatch getStatut() {
-        return statut;
-    }
-
-    public void setStatut(StatutMatch statut) {
-        this.statut = statut;
-    }
-
-    public VainqueurMatch getVainqueur() {
-        return vainqueur;
-    }
-
-    public void setVainqueur(VainqueurMatch vainqueur) {
-        this.vainqueur = vainqueur;
-    }
-
-    public String getStade() {
-        return stade;
-    }
-
-    public void setStade(String stade) {
-        this.stade = stade;
-    }
-
-    public boolean isDomicile() {
-        return domicile;
-    }
-
-    public void setDomicile(boolean domicile) {
-        this.domicile = domicile;
-    }
-
-    public TourMatch getTour() {
-        return tour;
-    }
-
-    public void setTour(TourMatch tour) {
-        this.tour = tour;
-    }
 }
